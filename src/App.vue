bffa7efc1f7e7216c27c33f0fa3954e05c25ef39
philheb <philippe.heb@gmail.com>
2018-11-23 14:07:38 -0800

    boilerplate /  initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+      <v-navigation-drawer temporary v-model="sideNav" fixed app>
+        <v-list dense>
+          <v-list-tile 
+          v-for="item in menuItems" 
+          :key="item.title"
+          router
+          :to="item.link">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      
+      <v-toolbar dark class="primary">
+        <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up">
+        </v-toolbar-side-icon>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn 
+          flat 
+          v-for="item in menuItems" 
+          :key="item.title"
+          router
+          :to="item.link">
+            <v-icon left>{{ item.icon }}</v-icon>
+            {{ item.title }}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+    
+    
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        { icon: 'people', title: 'View Meetups', link: '/meetups' },
+        { icon: 'meeting_room', title: 'Organize Meetup', link: '/meetup/new' },
+        { icon: 'person', title: 'Profile', link: '/profile' },
+        { icon: 'tag_faces', title: 'Sign up', link: '/signup' },
+        { icon: 'lock_open', title: 'Sign in', link: '/signin' }
+      ]
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#2196F3',
+    secondary: '#64B5F6',
+    accent: '#4CAF50',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#2196f3',
+    success: '#4caf50'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
