bae0d577f5ecf3d47181ecdc86a830f1faaa1dc9
philheb <philippe.heb@gmail.com>
2018-11-30 13:42:23 -0800

    dynamic UI

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,57 @@
 <template>
-    <div>
-        <p>The Create Meetup Page</p>
-    </div> 
+    <v-container>
+      <v-layout row>
+        <v-flex xs10 sm6 offset-xs1 offset-sm3>
+          <div class="headline">Create a new Meetup</div>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs12>
+          <form>
+            <v-layout row>
+              <v-flex xs10 sm6 offset-xs1 offset-sm3>
+                <v-text-field
+                  name="title"
+                  label="Title"
+                  id="title"
+                  required>
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs10 sm6 offset-xs1 offset-sm3>
+                <v-text-field
+                  name="location"
+                  label="Location"
+                  id="location"
+                  required>
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs10 sm6 offset-xs1 offset-sm3>
+                <v-text-field
+                  name="imageUrl"
+                  label="Image URL"
+                  id="image-url"
+                  required>
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs10 sm6 offset-xs1 offset-sm3>
+                <v-textarea
+                  name="description"
+                  label="Description"
+                  id="description"
+                  auto-grow
+                  rows="1"
+                  required>
+                </v-textarea>
+              </v-flex>
+            </v-layout>
+          </form>
+        </v-flex>
+      </v-layout>
+    </v-container> 
 </template>
